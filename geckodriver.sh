fc86c736f805bf0838a0c40add1331a793d0c95d
Boring-Mind <55541040+Boring-Mind@users.noreply.github.com>
2020-02-09 21:29:39 -0800

    Update geckodriver.sh

--- geckodriver.sh
+++ geckodriver.sh
@@ -1,4 +1,4 @@
-wget https://github.com/mozilla/geckodriver/releases/download/v0.xx.x/geckodriver-v0.xx.x-linux64.tar.gz
-sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.xx.x-linux64.tar.gz -O > /usr/bin/geckodriver'
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.26.0-linux64.tar.gz -O > /usr/bin/geckodriver'
 sudo chmod +x /usr/bin/geckodriver
 sudo ln -s /usr/bin/geckodriver /usr/local/bin/geckodriver
